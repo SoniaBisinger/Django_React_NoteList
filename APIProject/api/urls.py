9438d2e3eb5270efc19e4735d26fbaf7cc7abb54
Sonia Bisinger <sonia.bisinger@gmail.com>
2023-10-14 21:26:29 +0200

    Fixing API calls

--- APIProject/api/urls.py
+++ APIProject/api/urls.py
@@ -3,7 +3,7 @@ from .views import NoteList, NoteDetail
 
 urlpatterns = [
     path('notes/', NoteList.as_view(), name="notes"),
-    path('notes/<int:pk>/', NoteDetail.as_view(), name='note-detail'),
+    path('notes/<int:pk>', NoteDetail.as_view(), name='note-detail'),
 ]
 
 # /notes GET, POST
